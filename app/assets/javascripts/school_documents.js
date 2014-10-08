fdc935efea9328456a6eb845a0b701ed730ffd2e
Wik <xentisize@gmail.com>
2014-10-08 20:32:46 +0800

    Working on File Upload Button

--- app/assets/javascripts/school_documents.js
+++ app/assets/javascripts/school_documents.js
@@ -42,3 +42,64 @@ $(function() {
     }
   });
 });
+
+
+// Hide the solution input file fields
+$(function() {
+  // $("#solutions-upload-field").css("visibility", "hidden");
+  $("#solution-upload-button").click(function(evt) {
+    evt.preventDefault();
+    $("#solutions-upload-field").trigger("click");
+  });
+});
+
+// Show solution file name when there is change
+$(function() {
+  $("#solutions-upload-field").change(function() {
+    var files = document.getElementById("solutions-upload-field").files;
+    if (files.length > 0) {
+      $.each(files, function(idx, elm) {
+        var file = "<tr><td>" + elm.name + "</td><td><span class='" + idx + " solution-files glyphicon glyphicon-remove'></span></td></tr>";
+        $(file).insertAfter("table#solution-files-table tr:last");
+      });
+    }
+  });
+});
+
+$(function() {
+  $("#solution-reset-button").click(function(evt) {
+    console.log("Reset");
+    evt.preventDefault();
+    var solution_upload_template = $("#solutions-upload-field");
+    console.log(solution_upload_template);
+    $("#solutions-upload-field").replaceWith($(solution_upload_template).clone(true));
+    $("#solution-upload-button").click(function(evt) {
+      evt.preventDefault();
+      $("#solutions-upload-field").trigger("click");
+    });
+  });
+});
+// $(function() {
+//   $("table#solution-files-table").change(function() {
+//     console.log("Xxx");
+//     $(".solution-files").click(function() {
+//       console.log(this);
+//     });
+//   });
+// });
+//     // console.log(this);
+
+// $(function() {
+//   $("#solution-files-upload").change(function() {
+//     console.log("File selected");
+//     var solution_files = document.getElementById("solution-files-upload");
+//     if (solution_files.files.length > 0) {
+//       // $.each(solution_files.files, function(idx, elm) {
+//       //   // var solution_file ="<tr><td>" + elm.name + "</td></tr>";
+//       //   // $(solution_file).insertAfter(".solution-files-table:last");
+//       //   console.log(idx + ":" + elm.name);
+//       // });
+//       console.log("There are " + solution_files.files.length + " files");
+//     }
+//   });
+// });
